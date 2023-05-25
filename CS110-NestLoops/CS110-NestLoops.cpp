a413323c18b654373047ae78b0a5fa426e226700
Taylor Bisset <taylorbisset@gmail.com>
2023-05-25 10:29:19 -0700

    Added code that prints a grid of 9 rows, each with 9 columns, using nested loops.

--- CS110-NestLoops/CS110-NestLoops.cpp
+++ CS110-NestLoops/CS110-NestLoops.cpp
@@ -4,5 +4,16 @@ using namespace std;
 
 int main()
 {
-    
+    char horLn = 196, verLn = 179;
+    for (int row = 0; row < 10; row++) {
+        for (int row = 0; row <= 10 * 4; row++) {
+            cout << horLn;
+        }
+        cout << endl;
+        for (int column = 0; column < 10; column++) {
+            cout << verLn << row << ':' << column;
+        }
+        cout << verLn << endl;
+    }
+    cout << "\n\n";
 }
